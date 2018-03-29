9f06624be6bda5b80324e4eb20516fb321e09327
Steve McQuaid <steve@stevemcquaid.com>
2018-03-29 00:09:22 +0000

    Break out into resp headers

--- src/decoder.go
+++ src/decoder.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
 	"encoding/json"
 	"fmt"
-	//"net/http"
+	"net/http"
 	"math/big"
 )
 
@@ -34,11 +35,49 @@ func marshalFalco(falcoJson []byte){
 	fmt.Println(fr)
 }
 
+func handleFalco(w http.ResponseWriter, r *http.Request) {
+    //decoder := json.NewDecoder(req.Body)
+    //var fr Falco_Response
+    //err := decoder.Decode(&fr)
+    //if err != nil {
+        //panic(err)
+    //}
+
+    // Read body
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// Unmarshal
+	var fr Falco_Response
+	err = json.Unmarshal(b, &fr)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	s := "Success! "
+	output, err := json.Marshal(s)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Header().Set("content-type", "application/json")
+	w.Write(output)
+
+
+	//json.Unmarshal(bodyBytes, &fr)
+	//defer req.Body.Close()
+    fmt.Println(fr)
+}
+
 func main() {
-	//http.HandleFunc("/", parseGhPost)
-	//http.ListenAndServe(":80", nil)
-	falcoJson := `{"output":"18:37:22.181204909: Notice A shell was spawned in a container with an attached terminal (user=root k8s.pod=falco-6htpw container=5dea0c14015a shell=bash parent=<NA> cmdline=bash  terminal=34818)","priority":"Notice","rule":"Terminal shell in container","time":"2018-03-28T18:37:22.181204909Z", "output_fields": {"container.id":"5dea0c14015a","evt.time":1522262242181204909,"k8s.pod.name":"falco-6htpw","proc.cmdline":"bash ","proc.name":"bash","proc.pname":null,"proc.tty":34818,"user_name":"root"}}`
-	marshalFalco([]byte(falcoJson))
+	http.HandleFunc("/", handleFalco)
+	http.ListenAndServe(":80", nil)
+	//falcoJson := `{"output":"18:37:22.181204909: Notice A shell was spawned in a container with an attached terminal (user=root k8s.pod=falco-6htpw container=5dea0c14015a shell=bash parent=<NA> cmdline=bash  terminal=34818)","priority":"Notice","rule":"Terminal shell in container","time":"2018-03-28T18:37:22.181204909Z", "output_fields": {"container.id":"5dea0c14015a","evt.time":1522262242181204909,"k8s.pod.name":"falco-6htpw","proc.cmdline":"bash ","proc.name":"bash","proc.pname":null,"proc.tty":34818,"user_name":"root"}}`
+	//marshalFalco([]byte(falcoJson))
 }
 
 //func parseGhPost(rw http.ResponseWriter, request *http.Request) {
